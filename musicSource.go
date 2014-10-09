dc4257c1c4035e5d6ea51e05eaeded8a23d9c7ea
ledbutter <svengrosen@gmail.com>
2014-10-09 03:35:49 +0000

    initial commit, returns raw response body

---
+++ musicSource.go
@@ -0,0 +1,37 @@
+package musicSource
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type SearchCriteria struct {
+	ArtistName string
+	AlbumName  string
+}
+
+type Searcher interface {
+	Search() string
+}
+
+type MusicBrainzSearcher struct {
+	Criteria SearchCriteria
+}
+
+func (mbSearcher *MusicBrainzSearcher) Search() string {
+	//query := "http://musicbrainz.org/ws/2/artist?query=beatles&fmt=json"
+	query := "http://musicbrainz.org/ws/2/artist?query=" + mbSearcher.Criteria.ArtistName + "&fmt=json"
+	fmt.Println(query)
+	resp, err := http.Get(query)
+	if err != nil {
+		return "Uh Oh, Get()"
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "Uh oh, ReadAllBytes()"
+	}
+	content := string(body)
+	return content
+}
