b1b6531585479a2f7928c3d880de0c1c53d9e773
Sven Grosen <svengrosen@gmail.com>
2014-11-02 18:58:36 -0800

    switch to actually return an Album struct when searching

--- musicSource.go
+++ musicSource.go
@@ -2,51 +2,61 @@ package musicSource
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
 )
 
+// Represents the search criteria needed to search for an album.
 type SearchCriteria struct {
 	ArtistName string
 	AlbumName  string
 }
 
+// Interface for searching.
+// Only included so I can say I tried using an interface.
+// Also allows for more simple substitution of the MusicBrainzSearcher type, in theory.
 type Searcher interface {
-	Search() string
+	Search() (Album, error)
 }
 
+// Struct for searching musicbrainz for an album.
 type MusicBrainzSearcher struct {
 	Criteria SearchCriteria
 }
 
+// Represents a musical artist.
 type Artist struct {
 	Id   string
 	Name string
 }
 
+// Represents an album.
 type Album struct {
-	Title string
+	Id     string
+	Title  string
+	Artist string
 }
 
-func (mbSearcher *MusicBrainzSearcher) Search() string {
+// Searches MusicBrainz for an artist/album combination.
+func (mbSearcher *MusicBrainzSearcher) Search() (Album, error) {
 	/*
 		TODO: create sub-routine to handle executing query and extracting returnvalue
 	*/
+	album := Album{}
 	val := url.Values{}
 	val.Add("query", mbSearcher.Criteria.ArtistName)
 	val.Add("fmt", "json")
 	query := "http://musicbrainz.org/ws/2/artist?" + val.Encode()
 	resp, err := http.Get(query)
 	if err != nil {
-		return "Uh Oh, Get()"
+		return album, err
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "Uh oh, ReadAllBytes()"
+		return album, err
 	}
 
 	var result struct {
@@ -54,7 +64,7 @@ func (mbSearcher *MusicBrainzSearcher) Search() string {
 	}
 	err = json.Unmarshal(body, &result)
 	if err != nil {
-		return "Uh oh, Unmarshal"
+		return album, err
 	}
 
 	var artistId = result.Artists[0].Id
@@ -82,12 +92,12 @@ func (mbSearcher *MusicBrainzSearcher) Search() string {
 	var resp2 *http.Response
 	resp2, err = http.Get(query)
 	if err != nil {
-		return "Uh Oh, Album Get()"
+		return album, err
 	}
 	defer resp2.Body.Close()
 	body2, err2 := ioutil.ReadAll(resp2.Body)
 	if err2 != nil {
-		return "Uh Oh, Album ReadAllBytes()"
+		return album, err
 	}
 
 	/*
@@ -99,17 +109,8 @@ func (mbSearcher *MusicBrainzSearcher) Search() string {
 	err = json.Unmarshal(body2, &topLevel)
 	m := topLevel.(map[string]interface{})
 	albumHackResult := (m["releases"].([]interface{})[0]).(map[string]interface{})
-	foo := albumHackResult["title"].(string)
-	return foo
-	/*var result2 struct {
-		Albums [1]Album
-	}
-
-	err2 = json.Unmarshal(body2, &result2)
-	if err2 != nil {
-		return "Uh oh, Album Unmarshal"
-	}
-
-	fmt.Printf("%#v\n", result2.Albums[0])
-	return result2.Albums[0].Title*/
+	album.Title = albumHackResult["title"].(string)
+	album.Id = albumHackResult["id"].(string)
+	album.Artist = result.Artists[0].Name
+	return album, nil
 }
